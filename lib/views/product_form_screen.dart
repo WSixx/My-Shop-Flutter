879e73604866022f9d69a7b7e6dd137f95b19bb6
WSixx <lucas.2007s@gmail.com>
2021-03-04 23:29:47 -0300

    Focus

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -6,11 +6,37 @@ class ProductFormScreen extends StatefulWidget {
 }
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: 'Titulo',
+                ),
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_priceFocusNode),
+              ),
+              TextFormField(
+                focusNode: _priceFocusNode,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  labelText: 'Preço',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
