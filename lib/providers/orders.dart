79216b781c9715452c875717a48c4e5aaa048a3d
WSixx <lucas.2007s@gmail.com>
2021-03-13 11:11:24 -0300

    orders firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:my_shop/providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class Order {
   final String id;
@@ -13,6 +15,8 @@ class Order {
 }
 
 class Orders with ChangeNotifier {
+  final String _baseUrl = 'myshop-b8edb-default-rtdb.firebaseio.com';
+
   List<Order> _items = [];
 
   List<Order> get items {
@@ -23,12 +27,29 @@ class Orders with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
     //final total = products.fold(0.0, (t, i) => t + (i.price * i.quantity));
+    final date = DateTime.now();
+    final response = await http.post(
+      Uri.https(_baseUrl, 'orders.json'),
+      body: json.encode({
+        'total': cart.totalAmount,
+        'date': date.toIso8601String(),
+        'products': cart.items.values
+            .map((cartItem) => {
+                  'id': cartItem.id,
+                  'productId': cartItem.productId,
+                  'title': cartItem.title,
+                  'quantity': cartItem.quantity,
+                  'price': cartItem.price,
+                })
+            .toList(),
+      }),
+    );
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble.toString(),
+        id: json.decode(response.body)['name'],
         total: cart.totalAmount,
         date: DateTime.now(),
         products: cart.items.values.toList(),
--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,7 +20,26 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  Future<void> toggleFavorite() async {
+    _toggleFavorite();
+
+    try {
+      final String url = 'myshop-b8edb-default-rtdb.firebaseio.com';
+      final response = await http.patch(
+        Uri.https(url, 'products/$id.json'),
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } catch (error) {
+      _toggleFavorite();
+    }
+  }
+
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     notifyListeners();
   }
