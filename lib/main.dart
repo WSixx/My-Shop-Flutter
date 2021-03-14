477b2a8584ca072c0941e1bd8a9f24849785dbd7
WSixx <lucas.2007s@gmail.com>
2021-03-14 11:46:33 -0300

    auth screen 1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:my_shop/providers/cart.dart';
 import 'package:my_shop/providers/orders.dart';
 import 'package:my_shop/providers/products.dart';
 import 'package:my_shop/utils/app_routes.dart';
+import 'package:my_shop/views/auth_screen.dart';
 import 'package:my_shop/views/cart_screen.dart';
 import 'package:my_shop/views/orders_screen.dart';
 import 'package:my_shop/views/product_detail_screen.dart';
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         routes: {
+          AppRoutes.AUTH: (ctx) => AuthScreen(),
           AppRoutes.HOME: (ctx) => ProductsOverviewScreen(),
           AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
           AppRoutes.CART: (ctx) => CartScreen(),
